7fd700d6f3dc752cc7228af722e07bba1e74f39f
logixode <rohmad1230@gmail.com>
2020-08-20 22:46:09 +0700

    awkwards

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="data" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "App",
   components: {
     HelloWorld
+  },
+  data: () => ({
+    data: {}
+  }),
+  mounted() {
+    axios
+      .get("https://website-coba-api.000webhostapp.com/api/siswa")
+      .then(res => (this.data = res.data))
+      .catch(err => console.log(err));
   }
 };
 </script>
